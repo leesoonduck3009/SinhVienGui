dcb4af0499224facdf53dc76302e0fe2cce0da65
PNB309 <nguyen.phucbinh445@gmail.com>
2022-11-12 00:11:26 +0700

    12/11/2022

---
+++ GUI/FormMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenChildFrom(new FormList());
+            
+        }
+        private Form ActiveForm = null;
+        private void OpenChildFrom(Form FormChild)
+        {
+            if(ActiveForm != null)
+            {
+                ActiveForm.Close(); 
+            }
+            FormChild.TopLevel = false;
+            FormChild.FormBorderStyle = FormBorderStyle.None;
+            FormChild.Dock = DockStyle.Fill;
+            ActiveForm = FormChild;
+            pnlDisplay.Controls.Add(FormChild);
+            pnlDisplay.Tag = FormChild;
+            FormChild.BringToFront();
+            FormChild.Show();
+        }
+
+        private void bttFind_Click(object sender, EventArgs e)
+        {
+            OpenChildFrom(new FormSearch());
+        }
+
+        private void bttDel_Click(object sender, EventArgs e)
+        {
+            OpenChildFrom(new FormDel());
+        }
+
+        private void bttAdd_Click(object sender, EventArgs e)
+        {
+            OpenChildFrom(new FormAdd());
+        }
+
+        private void bttChange_Click(object sender, EventArgs e)
+        {
+            OpenChildFrom(new FormChange());
+        }
+    }
+   
+}
---
+++ GUI/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace GUI
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Avg { get; set; }
+        public string Class { get; set; }
+        public Student(int id, string name, float avg, string @class)
+        {
+            Id = id;
+            Name = name;
+            Avg = avg;
+            Class = @class;
+        }
+        public override string ToString()
+        {
+            return Id.ToString() + "/" + Name + "/" + Class + "/" + Avg.ToString() + "\n";
+        }
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            string[] ListStudent = File.ReadAllLines("data.txt");
+            for (int i = 0; i < ListStudent.Length; i++)
+            {
+                string[] listDataStudent = ListStudent[i].Split('/');
+                Student a = new Student(int.Parse(listDataStudent[0]), listDataStudent[1], float.Parse(listDataStudent[3]), listDataStudent[2]);
+                students.Add(a);
+            }
+            return students;
+        }
+        public Student()
+        { }
+        public static bool operator == (Student a, Student b)
+        {
+            if (a.Id == b.Id && a.Name == b.Name && a.Class == b.Class && a.Avg == b.Avg)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Student a, Student b)
+        {
+            if (a.Id != b.Id || a.Name != b.Name || a.Class != b.Class || a.Avg != b.Avg)
+                return true;
+            return false;
+        }
+    }
+}
